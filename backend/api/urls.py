04e5eddd0c8d431be1d085551df40e075134ddaa
Арсен Григорян <arsen8577711@yandex.ru>
2023-03-15 09:41:35 +0300

    Замечания по ревью

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,8 +13,8 @@ app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipes')
-router.register(r'recipes/(?P<recipes_id>\d+)/favorites/', FavoriteViewSet, basename='recipes')
-router.register(r'recipes/(?P<recipes_id>\d+)/shopping_cart/', ShoppingCartViewSet, basename='recipes')
+router.register(r'recipes/${id}/favorites/', FavoriteViewSet, basename='recipes')
+router.register(r'recipes/${id}/shopping_cart/', ShoppingCartViewSet, basename='recipes')
 router.register(r'tags', TagViewSet, basename='tags')
 router.register(r'users', CustomUserViewSet, basename='users')
 router.register(
